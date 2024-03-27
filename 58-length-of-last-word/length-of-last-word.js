972da9286dccccc9fa60e80ff5da96076975bd9e
Sarvesh <sarveshkumar574@gmail.com>
2024-03-27 17:06:05 +0530

    Time: 59 ms (15.51%) | Memory: 49.5 MB (7.93%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let c = 0
+    for(let i = s.length - 1 ; i >= 0; i--){
+        if(s[i] != ' '){
+            c++
+        }
+        if(c > 0 && s[i] == ' '){
+            break
+        }
+    }
+    console.log('c>',c)
+    return c
+}
