eeca9947e9e2991844755b8398f26ae3e679af8e
Sarvesh <sarveshkumar574@gmail.com>
2024-03-31 17:32:04 +0530

    Time: 66 ms (43.98%) | Memory: 51.1 MB (89.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -5,7 +5,7 @@
 var isPalindrome = function(s) {
     s = s.toLowerCase()
     s = s.replace(/[^a-zA-Z0-9]/g, '');
-    console.log(s)
+    //console.log(s)
     let l = 0 , r = s.length - 1, palindrom = true
     while(l < r){
         if(s[l] != s[r]){
